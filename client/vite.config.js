26ca4e2c20a8150e7db85c90bf0fe5573b38ee7c
MaherHazbaz <maherhazbaz7@gmail.com>
2024-09-22 10:26:10 +0530

    vitte

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-icons/io5"],
+      external: [], // Remove any externalization
     },
+  },
+  optimizeDeps: {
+    include: ["react-icons/io5"], // Explicitly include react-icons
   },
 });
